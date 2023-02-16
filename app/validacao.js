eacc27194f34fa97c0a4a503b94bf126d4aec95e
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-02-16 11:50:05 +0100

    js code
    finishing the js code but there is still some mistakes to fixe it

--- app/validacao.js
+++ app/validacao.js
@@ -13,6 +13,8 @@ function verificaSeOChutePossuiUmValorValido(chute) {
     document.body.innerHTML = `
         <h2>Você acertou!</h2>
         <h3>O número secreto era ${numeroSecreto}</h3>
+
+        <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
     `
   } else if (numero > numeroSecreto) {
     elementoChute.innerHTML += `
@@ -32,3 +34,9 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorQueOValorPermitido(numero) {
   return numero > maiorValor || numero < menorValor;
 }
+
+document.body.addEventListener('click', e => {
+    if(e.target.id === 'jogar-novamente') {
+        window.location.reload
+    }
+})
